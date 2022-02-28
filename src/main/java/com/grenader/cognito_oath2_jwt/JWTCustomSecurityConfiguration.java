085a343ca084915cf7ccfbc68835f4c1ecd11c48
Igor Kanshyn <igor.kanshyn@gmail.com>
2022-02-28 15:45:14 -0500

    adding tmp sample code

--- src/main/java/com/grenader/cognito_oath2_jwt/JWTCustomSecurityConfiguration.java
+++ src/main/java/com/grenader/cognito_oath2_jwt/JWTCustomSecurityConfiguration.java
@@ -31,6 +31,24 @@ public class JWTCustomSecurityConfiguration extends WebSecurityConfigurerAdapter
             }
             return authorities;
         }
+
+
+        /*
+        // from here: https://www.baeldung.com/spring-security-granted-authority-vs-role
+        private Collection<? extends GrantedAuthority> getAuthorities(
+                Collection<Role> roles) {
+            List<GrantedAuthority> authorities
+                    = new ArrayList<>();
+            for (Role role: roles) {
+                authorities.add(new SimpleGrantedAuthority(role.getName()));
+                role.getPrivileges().stream()
+                        .map(p -> new SimpleGrantedAuthority(p.getName()))
+                        .forEach(authorities::add);
+            }
+
+            return authorities;
+        }*/
+
     }
 
     @Override
